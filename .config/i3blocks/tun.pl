47158c30bb2182e8cd4570ff65ec8bbc82586a98
Sandy <thecsw@ku.edu>
2019-09-13 00:12:19 -0500

    Updated the i3blocks config

--- .config/i3blocks/tun.pl
+++ .config/i3blocks/tun.pl
@@ -1,4 +1,4 @@
 #!/bin/env perl
 
-print("VPN: up") if (system("test -e /sys/class/net/tun0") == 0);
-print("VPN: down") if (system("test -e /sys/class/net/tun0") != 0);
+print("VPN: up\n") if (system("test -e /sys/class/net/tun0") == 0);
+print("VPN: down\n") if (system("test -e /sys/class/net/tun0") != 0);
